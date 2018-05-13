451b6362fa282d2291b02f82b05cd4b9be7e1681
Viktoria <vikydragun@gmail.com>
2018-05-13 19:17:34 +0700

    Add file move.h

---
+++ src/move.h
@@ -0,0 +1,8 @@
+#ifndef MOVE_H_
+#define MOVE_H_
+
+void PawnMove (int y, int x, int y1, int x1, char (*A)[10]);
+void PawnMoveWhite (int y, int x, int y1, int x1, char (*A)[10]);
+void PawnMoveBlack (int y, int x, int y1, int x1, char (*A)[10]);
+
+#endif
